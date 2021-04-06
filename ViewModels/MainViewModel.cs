f749c79757984f2a4fd2aa986bafce2cd8a29be2
Manuel Bernacki <if19b010@technikum-wien.at>
2021-04-06 11:53:29 +0200

    Basic Layer Structure Implemented (DAL + DAO, Business Layer)

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -9,6 +9,7 @@ using TourManager.BusinessLayer;
 using TourManager.Commands;
 using TourManagerModels;
 using TourManager.Stores;
+using System.Collections;
 
 namespace TourManager.ViewModels
 {
@@ -16,11 +17,16 @@ namespace TourManager.ViewModels
     {        
         private readonly NavigationStore navigationStore;
         private ITourItemFactory tourItemFactory;
+
+        private string searchName;
         public ObservableCollection<Tour> TourItems { get; }
 
         public BaseViewModel CurrentViewModel => navigationStore.CurrentViewModel;
         public ICommand NavigateEditToursCommand { get; }
         public ICommand NavigateCreateToursCommand { get; }
+
+        public RelayCommand SearchCommand { get; }
+        public RelayCommand RefreshCommand { get; }
 
 
         public MainViewModel(NavigationStore navStore)
@@ -31,8 +37,41 @@ namespace TourManager.ViewModels
             NavigateEditToursCommand = new NavigateCommand<EditToursViewModel>(navigationStore, () => new EditToursViewModel(navigationStore));
             NavigateCreateToursCommand = new NavigateCommand<CreateToursViewModel>(navigationStore, () => new CreateToursViewModel(navigationStore));
 
+            SearchCommand = new RelayCommand(Search);
+            RefreshCommand = new RelayCommand(Refresh);
+
             this.tourItemFactory = TourItemFactory.GetInstance();
             this.TourItems = new ObservableCollection<Tour>();
+            FillTourItems();
+        }
+
+        public string SearchName
+        {
+            get { return searchName; }
+            set
+            {
+                if (searchName != value)
+                {
+                    searchName = value;
+                    OnPropertyChanged(nameof(SearchName));
+                }
+            }
+        }
+
+        private void Search(object commandParameter)
+        {
+            IEnumerable foundItem = this.tourItemFactory.Search(SearchName);
+            TourItems.Clear();
+            foreach(Tour t in foundItem)
+            {
+                TourItems.Add(t);
+            }
+        }
+
+        private void Refresh(object commandParameter)
+        {
+            TourItems.Clear();
+            SearchName = "";
             FillTourItems();
         }
 
